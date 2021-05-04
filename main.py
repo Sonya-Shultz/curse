68c5a15ec65672db4a4243d5cb5f68c2ae67dfea
Sonya-Shultz <55092783+Sonya-Shultz@users.noreply.github.com>
2021-05-05 02:06:14 +0300

    Separated big function, added function of flat light, changed lighting factors, etc.

--- main.py
+++ main.py
@@ -1,36 +1,54 @@
 import cv2
 import numpy as np
-import PIL
 from PIL import Image
 from PIL import ImageEnhance
 
 
-def test_img(img_name):
-    img = cv2.imread(img_name, cv2.IMREAD_GRAYSCALE)
-    img_start = cv2.imread(img_name)
+def find_all_contour(img_name):
     white_black(img_name, "res2.jpg", 0.85)
-    img = cv2.imread("res2.jpg",cv2.IMREAD_GRAYSCALE)
+    img = cv2.imread("res2.jpg", cv2.IMREAD_GRAYSCALE)
     blur = cv2.blur(img, (3, 3))  # blur the image
     ret, thresh = cv2.threshold(blur, 50, 255, cv2.THRESH_BINARY)
     contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
     hull = []
+
     # calculate points for each contour
     for i in range(len(contours)):
         hull.append(cv2.convexHull(contours[i], False))
+
     drawing = np.zeros((thresh.shape[0], thresh.shape[1], 3), np.uint8)
-    canvas = np.ones((img.shape[0], img.shape[1], 3), np.uint8)*100
+    canvas = np.ones((img.shape[0], img.shape[1], 3), np.uint8) * 100
+
     for i in range(len(contours)):
         color_contours = (0, 255, 0)
-        #color = (255, 0, 0)
         cv2.drawContours(drawing, contours, i, color_contours, 1, 8, hierarchy)
-        #cv2.drawContours(canvas, hull, i, color, 1, 8)
+
     cv2.imshow('img1', img)
     cv2.waitKey(0)
+
     cv2.imshow('countur', drawing)
     cv2.waitKey(0)
+    return contours, canvas
 
-    # Проходя через все контуры, найденные на изображении.
+
+def rotate_n_perspective_img(rotrect, box, page, img_start):
+    (x1, y1), (x2, y2), angle = rotrect
+    box1 = [[0, 0], [0, y2], [x2, y2], [x2, 0]]
+    box1 = forvard_back(box1)
+    box1 = max_x_sort(box1)
+    box = np.array(box, np.float32)
+    box1 = np.array(box1, np.float32)
+    page = aprox_in_array(find_near(page, box), page)
+    page = np.array(page, np.float32)
+    matrix = cv2.getPerspectiveTransform(page, box1)
+    result = cv2.warpPerspective(img_start, matrix, (int(x2), int(y2)))
+
+    cv2.imshow('img transform', result)
+    cv2.imwrite("res.jpg", result)
+    cv2.waitKey(0)
+
+
+def how_transform(contours, canvas):
     font = cv2.FONT_HERSHEY_COMPLEX
     page = []
     rotrect = cv2.minAreaRect(contours[0])
@@ -42,7 +60,7 @@ def test_img(img_name):
             i = 0
             for j in n:
                 if (i % 2 == 0):
-                    help_arr = [n[i], n[i+1]]
+                    help_arr = [n[i], n[i + 1]]
                     page.append(help_arr)
                     x = n[i]
                     y = n[i + 1]
@@ -50,65 +68,116 @@ def test_img(img_name):
                     cv2.putText(canvas, string, (x, y), font, 0.5, (0, 255, 0))
                 i = i + 1
             rotrect = cv2.minAreaRect(cnt)
-    # РЕДАГУВАННЯ МАСИВУ ДЛЯ ВІДПОВІДНОСТІ
-    # тут треба повороти
 
-    # коробочка по фрейду
+    # Зображення отриманого квадрату для тексту
     box = cv2.boxPoints(rotrect)
     box = np.int0(box)
     cv2.drawContours(canvas, [box], 0, (0, 255, 255), 2)
     cv2.imshow('box', canvas)
     cv2.waitKey(0)
-
-    # матриця переходу і трансформація
-    (x1, y1), (x2, y2), angle = rotrect
-    box1 = [[0, 0], [0, y2], [x2, y2], [x2, 0]]
-    box1 = forvard_back(box1)
-    box1 = max_X_sort(box1)
-    box = np.array(box, np.float32)
-    box1 = np.array(box1, np.float32)
-    page = aprox_in_array(find_near(page, box), page)
-    page = np.array(page, np.float32)
-    matrix = cv2.getPerspectiveTransform(page, box1)
-    result = cv2.warpPerspective(img_start, matrix, (int (x2), int(y2)))
+    return rotrect, box, page
 
 
-    # Wrap the transformed image
-    cv2.imshow('img transform', result)
-    cv2.imwrite("res.jpg", result)
+def transform_img(img_name):
+    img_start = cv2.imread(img_name)
+    cv2.imshow('img1', img_start)
     cv2.waitKey(0)
 
-    # висвітлення фону паперу, приберання малих косяків та підведення ліній
-    # increasing the contrast 20%
+    # Знаходимо контур листка
+    contours, canvas = find_all_contour(img_name)
+
+    # Проходимо всі контури та визначаємо матриці трансформації
+    rotrect, box, page = how_transform(contours, canvas)
+
+    # матриця переходу і трансформація
+    rotate_n_perspective_img(rotrect, box, page, img_start)
+
+
+def brightness_n_bw_img():
     image = Image.open("res.jpg")
+
+    # Збільшення яскравості на 20%
     new_image = ImageEnhance.Contrast(image).enhance(1.2)
     result = np.array(new_image)
     cv2.imwrite("res.jpg", result)
 
-    white_black("res.jpg", "res2.jpg", 0.85)
+    # перетворення в чб варіант та його читання
+    white_black("res.jpg", "res2.jpg", 0.6)
     result = cv2.imread("res2.jpg")
     cv2.imshow('bw-result', result)
     cv2.waitKey(0)
+    return result
 
-    cv2.destroyAllWindows()
 
-    # thickness
+def normalize_color_of_img(img_name):
+    img = cv2.imread(img_name)
+    blur = cv2.blur(img, (40, 40))
+    blur = cv2.bitwise_not(blur)
+    blur = np.array(blur)*0.6
+    img = calc_sum_color_rgb(img, blur, 0.5)
+    cv2.imwrite("res.jpg", img)
+    cv2.imshow('blur', img)
+    cv2.waitKey(0)
+
+
+def calc_sum_color_rgb(img1, img2, black_range):
+    for i in range(len(img1)):
+        for j in range(len(img1[i])):
+            help_color = np.copy(img1[i][j])
+            is_first = True
+            if (np.average(img1[i,j]) >= (black_range * 255)):
+                for x in range(len(img1[i][j])):
+                    help_div = img1[i, j, x] + img2[i, j, x]
+                    if help_div > 255:
+                        help_div = 255
+                    img1[i, j, x] = help_div
+            else:
+                for x in range(len(img1[i][j])):
+                    help_div = img1[i, j, x] + img2[i, j, x] * ((img1[i, j, x])/(black_range * 255))
+                    if help_div > 255:
+                        help_div = 255
+                    img1[i, j, x] = help_div
+    return img1
+
+
+def outline_line_on_img(result):
+    # читання в чб форматі і інверсія (для підсилення кольорів)
     gray = cv2.cvtColor(result, cv2.COLOR_BGR2GRAY)
     gray = cv2.bitwise_not(gray)
     cv2.imshow('gray invert', gray)
     cv2.waitKey(0)
 
+    # Пошук та виділення контурів для обведення букв
     edges = cv2.Canny(result, 50, 150, apertureSize=3)
     minLineLength = 1
-    maxLineGap = 1
+    maxLineGap = 30
     lines = cv2.HoughLinesP(edges, 1, np.pi / 180, 20, minLineLength, maxLineGap)
-    for l in lines:
-        for x1, y1, x2, y2 in l:
-            cv2.line(result, (x1, y1), (x2, y2), (0, 0, 0), 1)
+    if lines is not None:
+        for l in lines:
+            for x1, y1, x2, y2 in l:
+                cv2.line(result, (x1, y1), (x2, y2), (0, 0, 0), 1)
 
     cv2.imshow('with countur', result)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
+
+
+def test_img(img_name):
+    # Вирівнювання кольору працює доволі довго :'(
+    normalize_color_of_img(img_name)
+
+    # Стандартизація зображення
+    transform_img("res.jpg")
+
+    # це для складних випадків, коли результат не дуже нормальний
+    # transform_img("res.jpg")
+    # normalize_color_of_img("res.jpg")
+
+    # висвітлення фону паперу, приберання малих косяків
+    result = brightness_n_bw_img()
+
+    # підведення ліній
+    outline_line_on_img(result)
 
 
 def find_near(page, box):
@@ -162,7 +231,7 @@ def forvard_back(arr):
     return arr2
 
 
-def max_X_sort(arr):
+def max_x_sort(arr):
     arr2 = []
     max_s = 10000000000000000
     max_ind = 0
@@ -175,7 +244,6 @@ def max_X_sort(arr):
     for el in arr[0:max_ind]:
         arr2.append(el)
     return arr2
-
 
 
 def white_black(source_name, result_name, brightness):
@@ -193,6 +261,5 @@ def white_black(source_name, result_name, brightness):
     result.save(result_name, "JPEG")
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    test_img("test9.jpg")
+    test_img("test4.jpg")
